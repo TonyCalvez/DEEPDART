f3849bca6cc65b153ccb42123b736244251e6a79
Tony Calvez <tony.calvez@hotmail.fr>
2020-01-17 18:13:05 +0100

    20200117_1800

--- darknet/examples/camera_jetson.c
+++ darknet/examples/camera_jetson.c
@@ -2,21 +2,58 @@
 // Created by tonycalvez on 17/01/2020.
 //
 
-FILE *fp;
-char *command;
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//void camera_starter(){
+//    char filename[100];
+//    cout << "Enter file name to compile ";
+//    cin.getline(filename, 100);
+//
+//    // Build command to execute.  For example if the input
+//    // file name is a.cpp, then str holds "gcc -o a.out a.cpp"
+//    // Here -o is used to specify executable file name
+//    string str = "gcc ";
+//    str = str + " -o a.out " + filename;
+//
+//    // Convert string to const char * as system requires
+//    // parameter of type const char *
+//    const char *command = str.c_str();
+//
+//    cout << "Compiling file using " << command << endl;
+//    system(command);
+//
+//    cout << "\nRunning file ";
+//    system("./a.out");
+//
+//    return 0;
+//};
 
-/* command contains the command string (a character array) */
+// A C++ program that compiles and runs another C++
+// program
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    char filename[100];
+    cout << "JetsonCameraExporter";
+    cin.getline(filename, 100);
 
-/* If you want to read output from command */
-fp = popen(command,"r");
-/* read output from command */
-fscanf(fp,....);   /* or other STDIO input functions */
+    // Build command to execute.  For example if the input
+    // file name is a.cpp, then str holds "gcc -o a.out a.cpp"
+    // Here -o is used to specify executable file name
+    string str = "gst-launch-1.0 -v nvarguscamerasrc ! 'video/x-raw(memory:NVMM), format=NV12, width=1920, height=1080, framerate=30/1' ! nvvidconv ! 'video/x-raw, width=640, height=480, format=I420, framerate=30/1' ! videoconvert ! identity drop-allocation=1 ! 'video/x-raw, width=640, height=480, format=RGB, framerate=30/1' ! v4l2sink device=/dev/video2";
+    str = str + " -o a.out " + filename;
 
-fclose(fp);
+    // Convert string to const char * as system requires
+    // parameter of type const char *
+    const char *command = str.c_str();
 
-/* If you want to send input to command */
-fp = popen(command,"w");
-/* write to command */
-fprintf(fp,....);   /* or other STDIO output functions */
+    cout << "Compiling file using " << command << endl;
+    system(command);
 
-fclose(fp);
+    cout << "\nRunning file ";
+    system("./a.out");
+
+    return 0;
+}
